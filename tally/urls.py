6c0fb99fcbe4e99da23da15f4ca74778c728870c
James Kominick <james.kominick@gmail.com>
2015-12-29 17:06:46 -0500

    update views and add student templates

--- tally/urls.py
+++ tally/urls.py
@@ -8,10 +8,13 @@ urlpatterns = [
         url(r'^students/$', views.students, name='students'),
         url(r'^businesses/$', views.businesses, name='businesses'),
         url(r'^purchases/$', views.purchases, name='purchases'),
-        url(r'^students/(?P<student_name>[a-zA-Z0-9_]+)/$', 
+        url(r'^students/(?P<student_id>[a-zA-Z0-9_]+)/$', 
             views.student_detail, name='student_detail'),
 
         url(r'^businesses/(?P<business_name>[a-zA-Z0-9_]+)/$', 
             views.business_detail, name='business_detail'),
+        url(r'^purchases/(?P<purchase_id>[a-zA-Z0-9_]+)/$',
+            views.purchase_detail, name='purchase_detail'),
+        url(r'^add/student/$', views.add_student, name='add_student'),
         ]
 
--- tally/views.py
+++ tally/views.py
@@ -3,29 +3,41 @@ from django.shortcuts import get_object_or_404
 
 from django.http import HttpResponse as httpresp
 
+from .models import Student, Business, Purchase
 
 def index(request):
-    #return httpresp('welcome to the index!\n From here you can view students or businesses')
-    #context = {'my_response': 'generic response'}
     return render(request, 'tally/index.html')
 
 
+## Student Views
 def students(request):
-    return httpresp('welcome to the students index')
+    #return httpresp('welcome to the students index')
+    student_list = Student.objects.all()
+    context = {'student_list':student_list}
+    return render(request, 'tally/students.html', context)
+
+def student_detail(request, student_id):
+    student_name = Student.objects.get(pk=student_id).full_name
+    return httpresp('welcome to {}\'s student-detail view'.format(student_name))
+
+def add_student(request):
+    return render(request, 'tally/add_student.html')
 
 
+## Business Views
 def businesses(request):
     return httpresp('welcome to the business index')
 
+def business_detail(request, business_name):
+    return httpresp('welcome to {}\'s business-detail view'.format(business_name))
 
+
+## Purchase Views
 def purchases(request):
     return httpresp('welcome to the purchase index')
 
+def purchase_detail(request, purchase_id):
+    return httpresp('details for purchase id {}'.format(purchase_id))
 
-def student_detail(request, student_name):
-    return httpresp('welcome to {}\'s student-detail view'.format(student_name))
 
-
-def business_detail(request, business_name):
-    return httpresp('welcome to {}\'s business-detail view'.format(business_name))
 
